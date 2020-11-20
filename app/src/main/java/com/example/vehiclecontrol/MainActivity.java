880c85460ec9e9c13e208d6658e52768e942b6a8
senti50 <46302409+senti50@users.noreply.github.com>
2020-11-20 17:37:25 +0100

    added google map(add,delete,drag marker and draw line between markers)

--- app/src/main/java/com/example/vehiclecontrol/MainActivity.java
+++ app/src/main/java/com/example/vehiclecontrol/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.vehiclecontrol;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+
+import java.util.LinkedHashMap;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final String TAG = "MainActivity";
     private TextView xViewLeft;
     private TextView yViewLeft;
@@ -16,12 +30,21 @@ public class MainActivity extends AppCompatActivity {
     private JoyStick joyStickViewLeft;
     private JoyStick joyStickViewRight;
 
+
+    private GoogleMap mMap;
+
+    private LinkedHashMap<String, Marker> allPoints = new LinkedHashMap<>();
+    private Polyline polyline = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         joyStickViewLeft = findViewById(R.id.rockerView1);
         joyStickViewRight = findViewById(R.id.rockerView2);
@@ -52,11 +75,74 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    void doLog(String log) {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng).draggable(true);
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                Marker marker = mMap.addMarker(markerOptions);
+                allPoints.put(marker.getId(), marker);
+                drawLineBetweenMarkers();
+
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                allPoints.remove(marker.getId());
+                marker.remove();
+                drawLineBetweenMarkers();
+                return false;
+
+            }
+        });
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                Marker marker1 = allPoints.get(marker.getId());
+                if (marker1 != null) {
+                    marker1.setPosition(marker.getPosition());
+                }
+                drawLineBetweenMarkers();
+            }
+        });
+    }
+
+    private void drawLineBetweenMarkers() {
+        if (polyline != null) polyline.remove();
+        if (allPoints.size() > 1) {
+            PolylineOptions polylineOptions = new PolylineOptions();
+            polylineOptions.color(Color.BLACK);
+            for (Marker marker : allPoints.values()) {
+                polylineOptions.add(marker.getPosition());
+            }
+            polyline = mMap.addPolyline(polylineOptions);
+
+        }
+    }
+
+    private void doLog(String log) {
         Log.i(TAG, log);
     }
 
-    void changeCoordinatesOnView(TextView xTextView, TextView yTextView, float x, float y) {
+    private void changeCoordinatesOnView(TextView xTextView, TextView yTextView, float x, float y) {
         xTextView.setText("X: " + x);
         yTextView.setText("Y: " + y);
     }
