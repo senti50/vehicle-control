8217bf150ee7fea5d8ca4d0d8c3c2adb99a24817
senti50 <46302409+senti50@users.noreply.github.com>
2020-11-26 22:43:09 +0100

    added import/export points

--- app/src/main/java/com/example/vehiclecontrol/MainActivity.java
+++ app/src/main/java/com/example/vehiclecontrol/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.vehiclecontrol;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,9 +29,17 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, PopupMenu.OnMenuItemClickListener {
     private static final String TAG = "MainActivity";
     private TextView xViewLeft;
     private TextView yViewLeft;
@@ -39,15 +57,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        joyStickViewLeft = findViewById(R.id.rockerView1);
-        joyStickViewRight = findViewById(R.id.rockerView2);
+        joyStickViewLeft = findViewById(R.id.joyStickLeftView);
+        joyStickViewRight = findViewById(R.id.joyStickRightView);
 
         xViewLeft = findViewById(R.id.textView1);
         yViewLeft = findViewById(R.id.textView2);
@@ -74,6 +91,113 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    public void showMenu(View v) {
+
+        PopupMenu popup = new PopupMenu(this, v);
+
+        // This activity implements OnMenuItemClickListener
+        popup.setOnMenuItemClickListener((PopupMenu.OnMenuItemClickListener) this);
+        popup.inflate(R.menu.options);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.importPoints:
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("text/plain");
+                startActivityForResult(intent, 77);
+                return true;
+            case R.id.exportPoints:
+                exportPoints(getApplicationContext());
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void exportPoints(Context context) {
+        String filename = "Points";
+        String filepath = "ExportedPoints";
+
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyyHH:mm:ss", Locale.getDefault());
+        String currentDate = formatter.format(date);
+        try {
+            File myExternalFile = new File(getExternalFilesDir(filepath), filename + currentDate + ".txt");
+            FileOutputStream fos = new FileOutputStream(myExternalFile);
+
+            for (Marker mark : allPoints.values()) {
+                LatLng position = mark.getPosition();
+                fos.write(position.toString().getBytes());
+                fos.write(String.format("%n").getBytes());
+            }
+
+            fos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(this, "Points have been exported to the  \"ExportedPoints\" folder ", Toast.LENGTH_LONG).show();
+
+    }
+
+    private boolean readTextFile(Uri uri) {
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(getContentResolver().openInputStream(uri)));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                try {
+                    String replace = line.replace("lat/lng: (", "");
+                    String replace1 = replace.replace(")", "");
+                    String[] strings = replace1.split(",");
+                    double lat = Double.parseDouble(strings[0]);
+                    double lng = Double.parseDouble(strings[1]);
+                    LatLng latLng = new LatLng(lat, lng);
+                    MarkerOptions markerOptions = new MarkerOptions().position(latLng).draggable(true);
+                    Marker marker = mMap.addMarker(markerOptions);
+                    allPoints.put(marker.getId(), marker);
+                    drawLineBetweenMarkers();
+                    builder.append(line);
+                }catch (Exception e){
+                    return false;
+                }
+
+            }
+            reader.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 77) {
+            if (resultCode == RESULT_OK) {
+                if (data == null) return;
+
+                Uri uri = data.getData();
+                boolean isCorrect = readTextFile(uri);
+                if (isCorrect) {
+                    Toast.makeText(this, "Points have been imported", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Unable to import points, wrong file format", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
